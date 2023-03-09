4ac964648dc0e665b226e2841f77af687d5eafff
Tusif-Hassan-Rizvi <76469398+Tusif-Hassan-Rizvi@users.noreply.github.com>
2023-03-09 09:42:29 +0530

    first commit

---
+++ Source/Beginnercourse/Public/CPP_basic.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CPP_basic.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEGINNERCOURSE_API UCPP_basic : public UObject
+{
+	GENERATED_BODY()
+	
+};
